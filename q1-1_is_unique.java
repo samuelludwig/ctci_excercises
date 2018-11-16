2e7af38e1fcb625b2a894737228477804db08b47
samuelludwig <samuelludwig74@yahoo.com>
2018-11-16 08:51:38 -0500

    renamed file

---
+++ q1-1_is_unique.java
@@ -0,0 +1,67 @@
+/**
+ * Implement an algorithm to determine if a string has all unique characters.
+ */
+import java.util.*;
+import java.lang.*;
+
+public class IsUnique {
+
+    public static void main(String[] args) {
+        System.out.println(isErrorGivenWithNoString());
+        System.out.println(isStringProcessedCorrectly());
+        System.out.println(isStringIsUniqueReturningFalseOnNonUniqueLetter());
+        System.out.println(isStringIsUniqueReturningTrueWithAllUniqueLetters());
+    }
+
+    static String processInput(String s) {
+        if (s.equals("")) {
+            return "Error, invalid string!";
+        }
+
+        return s.toLowerCase();
+    }
+
+    static boolean stringIsUnique(String s) {
+
+        return true;
+    }
+
+    
+    /* --- TESTS --- */
+
+    // Test that an error message is given if no String is supplied
+    static boolean isErrorGivenWithNoString() {
+        if (processInput("").equals("Error, invalid string!")) {
+            return true;
+        }
+        return false;
+    }
+
+    // Test that the provided String is converted correctly into lowercase
+    static boolean isStringProcessedCorrectly() {
+        if (processInput("HEllO").equals("hello")) {
+            return true;
+        }
+        return false;
+    }
+
+
+    // Test that stringIsUnique() returns false when a non-unique letter is encountered
+    static boolean isStringIsUniqueReturningFalseOnNonUniqueLetter() {
+        if (stringIsUnique("Hello") == false) {
+            return true;
+        }
+
+        return false;
+    }
+    
+    // Test that stringIsUnique() returns true when a unique string is passed through
+    static boolean isStringIsUniqueReturningTrueWithAllUniqueLetters() {
+        if (stringIsUnique("mango") == true) {
+            return true;
+        }
+
+        return false;
+    }
+
+}
