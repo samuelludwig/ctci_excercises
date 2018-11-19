86a6acca899ad6f1e000331b36a572fd7a9deed8
samuelludwig <samuelludwig74@yahoo.com>
2018-11-19 13:21:21 -0500

    added C# version of IsUnique question

---
+++ IsUnique/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace IsUnique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsUniqueReturnsTrueWhenItShould());
+            Console.WriteLine(IsUniqueReturnsFalseWhenItShould());
+        }
+
+        static bool IsUnique(String s) 
+        {
+            char[] str = s.ToCharArray();
+            Array.Sort(str);
+            for (int i = 0; i < (str.Length - 1); i++) {
+                if (str[i] == str[i+1]) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        static bool IsUniqueReturnsTrueWhenItShould() {
+            if (IsUnique("orange") == true) {
+                return true;
+            }
+
+            return false;
+        }
+
+        static bool IsUniqueReturnsFalseWhenItShould() {
+            if (IsUnique("hello") == false) {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
