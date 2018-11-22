0ce7d7a24807e86fe90c23fe3b5465a93ca34974
samuelludwig <samuelludwig74@yahoo.com>
2018-11-22 10:48:43 -0500

    C# version of checkPermutation completed

---
+++ CheckPermutationCS/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace CheckPermutationCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(isCheckPermReturningTrueWhenStringsArePermutations());
+            Console.WriteLine(isCheckPermReturningFalseWhenStringsAreNotPermutations());
+        }
+
+        static bool checkPermutation(String str1, String str2) {
+        if (str1.Length != str2.Length) {
+            return false;
+        }
+
+        char[] firstStr = str1.ToCharArray();
+        char[] secondStr = str2.ToCharArray();
+
+        Array.Sort(firstStr);
+        Array.Sort(secondStr);
+
+        for (int i = 0; i < str1.Length; i++) {
+            if (firstStr[i] != secondStr[i]) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+
+    
+    /* --- --- TESTS --- --- */
+
+    static bool isCheckPermReturningTrueWhenStringsArePermutations() {
+        if(checkPermutation("barrel", "rerlab") == true) {
+            return true;
+        }
+        
+        return false;
+    }
+
+    static bool isCheckPermReturningFalseWhenStringsAreNotPermutations() {
+        if(checkPermutation("racecar", "orange") == false) {
+            return true;
+        }
+        
+        return false;
+    }
+    }
+}
