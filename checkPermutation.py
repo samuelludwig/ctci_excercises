2b35eac17c7609064da6b65af19dabb638c00f3c
samuelludwig <samuelludwig74@yahoo.com>
2019-01-07 09:17:39 -0500

    adds checkPermutation.py, and restructures file setup for gitrepo

---
+++ checkPermutation.py
@@ -0,0 +1,31 @@
+"""Check if two strings are permutations of one another
+"""
+
+def check_permutation(string1, string2):
+    s1 = ''.join(sorted(string1))
+    s2 = ''.join(sorted(string2))
+
+    if s1 == s2:
+        return True
+    else:
+        return False
+
+"""TESTS"""
+
+def returns_false_if_not_permutations():
+    if check_permutation("door", "racecar") == False:
+        return 1
+    else:
+        return 0
+
+def returns_true_if_permutations():
+    if check_permutation("door", "odor") == True:
+        return 1
+    else:
+        return 0
+    
+def test_check_permutation():
+    print(returns_false_if_not_permutations())
+    print(returns_true_if_permutations())
+
+test_check_permutation()
