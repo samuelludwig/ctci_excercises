f3c8f330db503b7a639451cd339c7c66b6d55744
samuelludwig <samuelludwig74@yahoo.com>
2019-01-10 07:53:56 -0500

    finishes urlify.py
    This on was very different than the java and c# variants, the more I use it, it seems more intuitive, but then theres an element of 'why can't you just be normal?'

--- urlify.py
+++ urlify.py
@@ -3,15 +3,30 @@
     You may assume that the string has sufficient space at the end to hold the additional characters, and that you are given
     the true length of the string.
 """
+def preprocess(string):
+    preprocessed_string = ''.join(string.strip())
+    return preprocessed_string
 
-def urlify(string, length):
+def get_space_locations(string):
+    list_of_space_locations = []
     for i in range(string.__len__()):
         if string[i] == ' ':
-            for x in range(string.__len__()-i, 0, -1):
-                string[x] = string[x-3]
-        string[i] = '%'
-        string[i+1] = '2'
-        string[i+2] = '3'
+            list_of_space_locations.append(i)
+    return list_of_space_locations
+
+def urlify(string, length):
+    ps = ''.join(preprocess(string))
+    sp_loc = get_space_locations(ps)
+    url = []
+    char_position = 0
+    for char in ps:
+        if char_position in sp_loc:
+            url.append("%20")
+        else:
+            url.append(char)
+        char_position += 1
+    built_url = ''.join(url)
+    return built_url
 
 """TESTS"""
 
@@ -22,7 +37,19 @@ def is_returning_same_string_if_no_spaces():
         return 0
 
 def is_returning_urlified_string():
-    if urlify("who am i    ", 12) == "who%20am%20i%20":
+    if urlify("who am i    ", 12) == "who%20am%20i":
+        return 1
+    else:
+        return 0
+
+def preprocess_returns_stripped_string():
+    if preprocess("    i am you      ") == "i am you":
+        return 1
+    else:
+        return 0
+
+def get_space_locations_returns_list_of_locations():
+    if get_space_locations("that is nonsense") == [4, 7]:
         return 1
     else:
         return 0
@@ -30,5 +57,7 @@ def is_returning_urlified_string():
 def test_urlify():
     print(is_returning_same_string_if_no_spaces())
     print(is_returning_urlified_string())
+    print(preprocess_returns_stripped_string())
+    print(get_space_locations_returns_list_of_locations())
 
 test_urlify()
