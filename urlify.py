e5073df3de94a08add0a7ca20240d83f60a726ea
samuelludwig <samuelludwig74@yahoo.com>
2019-01-09 22:41:11 -0500

    adds further progress on urlify.py

--- urlify.py
+++ urlify.py
@@ -4,8 +4,31 @@
     the true length of the string.
 """
 
-def urlify(string):
-    pass
+def urlify(string, length):
+    for i in range(string.__len__()):
+        if string[i] == ' ':
+            for x in range(string.__len__()-i, 0, -1):
+                string[x] = string[x-3]
+        string[i] = '%'
+        string[i+1] = '2'
+        string[i+2] = '3'
 
 """TESTS"""
 
+def is_returning_same_string_if_no_spaces():
+    if urlify("carrot", 6) == "carrot":
+        return 1
+    else:
+        return 0
+
+def is_returning_urlified_string():
+    if urlify("who am i    ", 12) == "who%20am%20i%20":
+        return 1
+    else:
+        return 0
+
+def test_urlify():
+    print(is_returning_same_string_if_no_spaces())
+    print(is_returning_urlified_string())
+
+test_urlify()
