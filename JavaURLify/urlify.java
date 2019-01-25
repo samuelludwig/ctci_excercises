de4de77d4db3c8d968100e3c8c0029b662daf691
samuelludwig <samuelludwig74@yahoo.com>
2019-01-24 21:38:18 -0500

    adds java version of urlify

---
+++ JavaURLify/urlify.java
@@ -0,0 +1,17 @@
+/**
+ *  Write a method to replace all spaces in a string with '%20'. 
+ *  You may assume that the string has sufficient space at the end to hold the additional characters, and that you are given
+ *  the true length of the string.
+ */
+public class urlify {
+
+    public static void main(String[] args) {
+        
+    }
+
+    public String urlifyString() {
+        return 0;
+    }
+
+    
+}
