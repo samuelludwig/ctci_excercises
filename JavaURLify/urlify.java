7a4c034d23352798bbe859fdeae18230dfda4e86
samuelludwig <samuelludwig74@yahoo.com>
2019-01-25 21:49:32 -0500

    adds more progress to java version of urlify

--- JavaURLify/urlify.java
+++ JavaURLify/urlify.java
@@ -6,12 +6,52 @@
 public class urlify {
 
     public static void main(String[] args) {
-        
+        System.out.println(urlifyReturnsUrlifiedStringWhenSpacesPresent());
+        System.out.println(urlifyReturnsEmptyStringIfEmptyStringIsProvided());
+        System.out.println(urlifyReturnsUnalteredStringIfNoSpaces());
     }
 
-    public String urlifyString() {
-        return 0;
+    public static String urlifyString(String url, int tLength) {
+        char[] urlArr = url.toCharArray();
+        for (int i = 0; i < tLength; i++) {
+            if (urlArr[i] == ' ') {
+                for (int j = (urlArr.length - 1); j > i; j--) {
+                    urlArr[j] = urlArr[j-2];
+                }
+                urlArr[i] = '%';
+                urlArr[i+1] = '2';
+                urlArr[i+2] = '0';
+            }
+        }
+        // for (int i = tLength; i > 0; i--) {
+        //     urlArr[i] = urlArr[i-2];
+        // }
+        System.out.println(urlArr);
+        return urlArr.toString().trim();
     }
 
+    // TESTS //
+    static boolean urlifyReturnsUrlifiedStringWhenSpacesPresent() {
+        if (urlifyString("c a t    ", 5).equals("c%20a%20t")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    static boolean urlifyReturnsEmptyStringIfEmptyStringIsProvided() {
+        if (urlifyString("", 0).equals("")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
     
+    static boolean urlifyReturnsUnalteredStringIfNoSpaces() {
+        if (urlifyString("cat", 3).equals("cat")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
