8a4a2421f12e661f645d2bf6f579cb428856758d
Samuel Ludwig <samuelludwig74@yahoo.com>
2019-02-11 18:51:45 -0500

    adds serialization function to turn a list into a 2d array (our matrix)

--- rotate_matrix.py
+++ rotate_matrix.py
@@ -53,8 +53,28 @@ def up(num, dim):
 def down(num, dim):
     return (num+dim)
 
+def swap(my_list, index1, index2):
+    temp = my_list[index1]
+    my_list[index1] = my_list[index2]
+    my_list[index2] = temp
+
+def parse_to_matrix(my_list):
+    dim = int(math.sqrt(my_list.__len__()))
+    matrix = []
+    for i in range(0, dim, 1):
+        matrix.append([])   # I now have an empty matrix with the same 
+                            # amount of spots as I do items in my_list
+    for i in range(0, dim, 1):
+        for j in range(0, dim, 1):
+            matrix[i].append(my_list[j])
+        for x in range(0, dim, 1):
+            del my_list[0]
+    return matrix
+
 def rotate(this_matrix):
     matrix_dim = math.sqrt(this_matrix.__len__())
+    for i in range(4):
+        pass
 
 
 """ TEST METHODS """
@@ -71,7 +91,14 @@ def is_rotated_even_dim():
         return 1
     else:
         return 0
+    
+def parse_to_matrix_works():
+    if (parse_to_matrix([0, 1, 2, 3, 4, 5, 6, 7, 8]) == [[0, 1, 2], [3, 4, 5], [6, 7, 8]]):
+        return 1
+    else:
+        return 0
 
 if __name__ == "__main__":
     print(is_rotated_odd_dim())
     print(is_rotated_even_dim())
+    print(parse_to_matrix_works())
